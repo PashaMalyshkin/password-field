74e29f5b0d599ca3e7b552d33fe0df136d7e3e07
Pasha <pashamalyshkin2003@gmail.com>
2023-07-02 20:05:09 +0300

    changed strength sections layout

--- src/components/password-field/password-field.tsx
+++ src/components/password-field/password-field.tsx
@@ -17,59 +17,61 @@ export const PasswordField: FC = () => {
   } = useValidationPassword();
 
   return (
-    <div className="password-field">
-      <div className="password-field__container">
-        <form className="password-field__form">
+    <div className="container">
+      <div className="password-field">
+        <div className="password-field__container">
+          <form className="password-field__form">
 
-          <input
-            type={inputType}
-            name="password"
-            className="password-field__input"
-            value={password}
-            placeholder="Type a password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            type="button"
-            className="password-field__button"
-            onClick={() => setShowPassword(!showPassword)}
+            <input
+              type={inputType}
+              name="password"
+              className="password-field__input"
+              value={password}
+              placeholder="Type a password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="password-field__button"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <PiEyeLight size={24} />
+              ) : (
+                <PiEyeSlashLight size={24} />
+              )}
+            </button>
+          </form>
+          <div className="password-field__info">
+            {`${password.length} characters containing`}
+          </div>
+        </div>
+
+        <div className="password-strength">
+          <div className={cn(
+            'password-strength__item',
+            { red: isPasswordWeak || (!hasValidLength && password) },
+            { yellow: isPasswordMedium },
+            { green: isPasswordStrong },
+          )}
           >
-            {showPassword ? (
-              <PiEyeLight size={24} />
-            ) : (
-              <PiEyeSlashLight size={24} />
-            )}
-          </button>
-        </form>
-        <div className="password-field__info">
-          {`${password.length} characters containing:`}
-        </div>
-      </div>
-
-      <div className="password-strength">
-        <div className={cn(
-          'password-strength__item',
-          { red: isPasswordWeak || (!hasValidLength && password) },
-          { yellow: isPasswordMedium },
-          { green: isPasswordStrong },
-        )}
-        >
-        </div>
-        <div className={cn(
-          'password-strength__item',
-          { yellow: isPasswordMedium },
-          { red: !hasValidLength && password },
-          { green: isPasswordStrong },
-        )}
-        >
-        </div>
-        <div className={cn(
-          'password-strength__item',
-          { green: isPasswordStrong },
-          { red: !hasValidLength && password },
-          { green: isPasswordStrong },
-        )}
-        >
+          </div>
+          <div className={cn(
+            'password-strength__item',
+            { yellow: isPasswordMedium },
+            { red: !hasValidLength && password },
+            { green: isPasswordStrong },
+          )}
+          >
+          </div>
+          <div className={cn(
+            'password-strength__item',
+            { green: isPasswordStrong },
+            { red: !hasValidLength && password },
+            { green: isPasswordStrong },
+          )}
+          >
+          </div>
         </div>
       </div>
     </div>
